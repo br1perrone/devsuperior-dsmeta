9951f68f2ecb5a98cbc643ce090427583baa6208
br1perrone <brunoperrone07@gmail.com>
2022-09-10 23:41:05 -0300

    Twilio SMS

---
+++ backend/src/main/java/com/br1perrone/dsmeta/services/SmsService.java
@@ -0,0 +1,50 @@
+package com.br1perrone.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.br1perrone.dsmeta.entities.Sale;
+import com.br1perrone.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String monthYear = sale.getDate().getMonth() + "/" + sale.getDate().getYear();
+		
+		String msg = "O vendedor " + sale.getSellerName() +
+				" foi destaque em " + monthYear +
+				" com um total de R$ " + String.format("%.2f", sale.getAmount());
+		
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
